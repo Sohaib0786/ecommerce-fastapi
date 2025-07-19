b1fe3a154328014eb293535e7165a1c6948e02e6
Sohaib0786 <shoaibqu7714@gmail.com>
2025-07-19 15:11:07 +0530

    deploy changes1

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from routes import product_routes, order_routes
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "FastAPI E-Commerce Backend is running 🎉"}
+
 app.include_router(product_routes.router)
 app.include_router(order_routes.router)
-
